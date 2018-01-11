93f8fd9775b9081c5cf3b7441f5929ab6b1373c5
Ross Nelson <Rossn430@gmail.com>
2018-01-11 12:02:41 -0600

    Created RandomSearch and StrategicSearch classes. Implemented reading data from input.txt and creating various "games" from it in setup method.

--- src/Battleship.java
+++ src/Battleship.java
@@ -8,41 +8,74 @@ import java.util.StringTokenizer;
 
 public class Battleship {
 	private SearchBehavior searchBehavior;
-	private static int[] input;
-	public int[][] grid;
+	private static int[] input = new int[48]; 	// There are 48 integers that make up the 24 coordinates necessary to set up 3 games
+	private static int[][] grid;
 
 	// Sets default search strategy
 	public Battleship() {
 		searchBehavior = new HorizontalSearch();
 	}
 	
+	public static void main (String[] args) {
+		readInputFile();
+		
+		// Sets up grid in game one formation
+		setup(grid, 0, 16);
+		
+		// Sets up grid in game two formation
+		setup(grid, 16, 32);
+		
+		// Sets up grid in game three formation
+		setup(grid, 32, 48);
+	}
+
 	// Reads and saves data from input.txt to input
 	public static void readInputFile() {
-		String fileName = "input.txt";
 		try {
-			FileReader inputstream = new FileReader(fileName);
-			BufferedReader reader = new BufferedReader(inputstream);
-			String game;
+			FileReader inputFile = new FileReader("input.txt");
+			BufferedReader reader = new BufferedReader(inputFile);
+			String line;
 			int i = 0;
-			while ((game = reader.readLine()) != null) {
-				String[] sp = game.split("(");
-				input[i] = Integer.parseInt(sp[0]);
-				i++;
+			while ((line = reader.readLine()) != null) {
+				StringTokenizer tokenizer = new StringTokenizer(line,"(,)");
+				while(tokenizer.hasMoreTokens()) {
+					input[i] = Integer.parseInt(tokenizer.nextToken());
+					i++;
+				}
 			}
 			reader.close();
-			inputstream.close();
-			//
-			for (int l:input) {
-				System.out.println(l);
-			}
-			//
+			inputFile.close();
+//			// Prints the values stored in input
+//			for (int l:input) {
+//				System.out.print(l);
+//			}
+//			System.out.println("");
+//			//
 		} catch (Exception e) {
-			System.out.print(e);
+			e.printStackTrace();
 		}
 	}
 	
-	public static void main (String[] args) {
-		readInputFile();
+	// Sets up grid, g, with the data from input from the min value to the max value
+	public static void setup(int[][] g, int min, int max) {
+		grid = new int[25][25];
+		int x = 0, y = 0;
+		for (int i = min; i < max - 1; i += 2) {
+			x = input[i];
+			y = input[i + 1];
+			grid[x][y] = 1;
+//			// Prints the x, y coordinates that are being stored in grid
+//			System.out.println(x + "," + y);
+//			//
+		}
+//		// Prints the values stored in grid
+//		for (int[] n : grid) {
+//			for (int m : n) {
+//				System.out.print(m);
+//			}
+//			System.out.println("");
+//		}
+//		System.out.println("");
+//		//
 	}
-
 }
---
+++ src/RandomSearch.java
@@ -0,0 +1,22 @@
+
+public class RandomSearch implements SearchBehavior{
+
+	@Override
+	public void search(int[][] g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int cellsSearched() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
